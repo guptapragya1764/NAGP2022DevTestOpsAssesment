c6bb80060be1a154ae539169071d7d4f1260423b
Pragya Gupta <guptapragya1764@gmail.com>
2023-03-02 12:40:04 +0530

    updated files

---
+++ src/test/java/com/devopssample/service/NewSeleniumTest.java
@@ -0,0 +1,30 @@
+package com.devopssample.service;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewSeleniumTest {
+
+  WebDriver driver;
+
+  @Test()
+  public void amazonTest() {
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    driver.get("https://www.amazon.in/");
+    driver.findElement(By.id("twotabsearchtextbox")).sendKeys("phone");
+    driver.findElement(By.id("nav-search-submit-button")).click();
+    String text = driver.findElement(By.xpath("//*[@class='sg-col-inner']/div/span[3]")).getText();
+    Assert.assertEquals(text,"phone");
+  }
+
+
+  @Test()
+  public void googleTest() {
+
+  }
+}
